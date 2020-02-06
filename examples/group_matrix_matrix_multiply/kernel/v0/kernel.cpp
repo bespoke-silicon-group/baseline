b5e2e1dc58b492869682199a780b783d4b057791
Dustin Richmond <dustinar@uw.edu>
2020-02-06 12:24:58 -0800

    Added group matrix multiply implementation

---
+++ examples/group_matrix_matrix_multiply/kernel/v0/kernel.cpp
@@ -0,0 +1,46 @@
+/*
+ * This kernel performs matrix multiplication.
+ *
+ * This is the most basic version of single-tile Matrix-Matrix (MM)
+ * Multiplication. Unlike later versions, the statistics of first
+ * iteration of the MM computation is not discarded (i.e. the same tag
+ * is used for every iteration). See v1 for comparison.
+ */
+
+// BSG_TILE_GROUP_X_DIM and BSG_TILE_GROUP_Y_DIM must be defined
+// before bsg_manycore.h and bsg_tile_group_barrier.h are
+// included. bsg_tiles_X and bsg_tiles_Y must also be defined for
+// legacy reasons, but they are deprecated.
+#define BSG_TILE_GROUP_X_DIM 2
+#define BSG_TILE_GROUP_Y_DIM 2
+#define bsg_tiles_X BSG_TILE_GROUP_X_DIM
+#define bsg_tiles_Y BSG_TILE_GROUP_Y_DIM
+#include <bsg_manycore.h>
+#include <bsg_tile_group_barrier.h>
+#include <cstdint>
+
+#include <matrix_multiply.hpp>
+
+INIT_TILE_GROUP_BARRIER(r_barrier, c_barrier,
+                        0, BSG_TILE_GROUP_X_DIM-1,
+                        0, BSG_TILE_GROUP_Y_DIM-1);
+
+
+extern "C" {
+        int  __attribute__ ((noinline)) kernel_matrix_multiply(
+                      float *A, float *B, float *C,
+                      uint32_t A_HEIGHT, uint32_t A_WIDTH, uint32_t B_WIDTH,
+                      uint32_t block_size_y, uint32_t block_size_x) {
+                int rc;
+
+                bsg_cuda_print_stat_start(0);
+                rc = matrix_multiply_group(A, B, C, 
+                                           A_HEIGHT, A_WIDTH, B_WIDTH,
+                                           block_size_y, block_size_x);
+                bsg_cuda_print_stat_end(0);
+
+                bsg_tile_group_barrier(&r_barrier, &c_barrier);
+
+                return rc;
+        }
+}
