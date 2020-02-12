d11a9bc574b566f1d472ef7b457aa449e8b6519c
Neil Adit <neiladit@gmail.com>
2020-02-11 20:10:21 -0500

    Adding v3 files

--- examples/tile_vector_add/kernel/include/vector_add.hpp
+++ examples/tile_vector_add/kernel/include/vector_add.hpp
@@ -13,9 +13,32 @@ template <typename TA, typename TB, typename TC>
 int __attribute__ ((noinline)) kernel_tile_vector_add(TA *A, TB *B, TC *C,
                       uint32_t WIDTH) {
         // A single tile performs the entire vector addition
-	for (int iter_x = 0; iter_x < WIDTH; iter_x += 1) { 
+	/*for (int iter_x = 0; iter_x < WIDTH; iter_x += 1) { 
                 C[iter_x] = A[iter_x] + B[iter_x];
-	}
+	}*/
+        float a_[4],b_[4],c_[4];
+        for (int iter_x = 0; iter_x < WIDTH; iter_x += 4) { 
+                #pragma GCC unroll 4
+                for (int i = 0; i < 4; i++)
+                {
+                        a_[i] = A[iter_x+i];
+                }
+                #pragma GCC unroll 4
+                for (int i = 0; i < 4; i++)
+                {
+                        b_[i] = B[iter_x+i];
+                }
+                #pragma GCC unroll 4
+                for (int i = 0; i < 4; i++)
+                {
+                        c_[i] = a_[i]+b_[i];
+                }
+                #pragma GCC unroll 4
+                for (int i = 0; i < 4; i++)
+                {
+                        C[iter_x+i] = c_[i];
+                }
+        }
 
         return 0;
 }
