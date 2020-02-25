71aed4e6239594284dac6a4a2d63ccda2c26f851
Max Ruttenberg <mrutt@cs.washington.edu>
2020-02-25 14:32:26 -0800

    baseline code written

--- examples/graphs/bfs/bfs.cpp
+++ examples/graphs/bfs/bfs.cpp
@@ -6,22 +6,185 @@
 #include <typeinfo>
 #include <bsg_manycore_errno.h>
 #include <bsg_manycore_cuda.h>
+#include <bsg_manycore_printing.h>
 #include "../../common.h"
 #include <graph_generation/graph.hpp>
 #include <graph_generation/csr_blob.hpp>
+#include <graph_algorithm/BFS.hpp>
+#include <graph_formats/graph.hpp>
+#include <graph_formats/vertexset.hpp>
+#include <cuda_lite_applications/breadth_first_search.hpp>
+#include <cuda_lite_extensions/cuda_call.hpp>
+
+static int cuda_memcpy_to_device
+(hb_mc_device_t *device, hb_mc_dma_htod_t *jobs, int count)
+{
+    for (int i = 0; i < count; i++) {
+	hb_mc_dma_htod_t *job = &jobs[i];
+	printf("%s: Copying job %d\n", __func__, i);
+	CUDA_CALL(hb_mc_device_memcpy(device,
+				      (void*)job->d_addr,
+				      job->h_addr,
+				      job->size,
+				      HB_MC_MEMCPY_TO_DEVICE));
+    }
+    return HB_MC_SUCCESS;
+}
+
+static int cuda_memcpy_to_host
+(hb_mc_device_t *device, hb_mc_dma_dtoh_t *jobs, int count)
+{
+    for (int i = 0; i < count; i++) {
+	hb_mc_dma_dtoh_t *job = &jobs[i];
+	printf("%s: Copying job %d\n", __func__, i);
+	CUDA_CALL(hb_mc_device_memcpy(device,
+				      job->h_addr,
+				      (const void*)job->d_addr,
+				      job->size,
+				      HB_MC_MEMCPY_TO_HOST));
+    }
+    return HB_MC_SUCCESS;
+}
+
+#include <cuda_lite_extensions/cuda_call.hpp>
+#include <misc/array_size.h>
 
 using namespace formats;
 using namespace graph_generation;
+using namespace graph_algorithm;
 
-csr_blob_header_t * GRAPH = nullptr;
+static constexpr char KERNEL [] 	= "kernel";
+static constexpr hb_mc_dimension_t GRID = {2,2};
+static constexpr hb_mc_dimension_t TG	= {2,2};
+
+static constexpr std::size_t DMA_SIZE_THRESHOLD = 4 * 1024;
+
 
 int kernel_bfs(int argc, char *argv[])
 {
-    GRAPH = make_csr_blob(10, 1);
-    printf("generated a graph with %d nodes and %d edges\n",
-	   GRAPH->n_nodes, GRAPH->n_edges);
+    // generate graph and input frontier
+    CSRGraph graph_h(20, 4);
+    CSRGraph graph_b = graph_h;
+    
+    printf("Generated a graph %zu byte  graph with %d nodes %d edges\n",
+	   graph_h.size_in_bytes(), graph_h.n_nodes(), graph_h.n_edges());
+
+    DenseVertexSet  frontier_out_h(graph_h.n_nodes());
+    auto frontier_in_h = SparseVertexSet::RandomFromGraph(graph_h, graph_h.n_nodes()/20);
+    auto visited_h = DenseVertexSet::FromSparse(frontier_in_h);
+
+    // cuda init
+    hb_mc_device_t CUDA;
+    CUDA_CALL(hb_mc_device_init_custom_dimensions(&CUDA, "bfs", 0, hb_mc_dimension(2,2)));
+
+    printf("Loading '%s' on device\n", argv[1]);
+    CUDA_CALL(hb_mc_device_program_init(&CUDA, argv[1], "bfs", 0));
+
+    // allocate graph data
+    hb_mc_eva_t graph_d, frontier_in_d, frontier_out_d, visited_d;
+    CUDA_CALL(hb_mc_device_malloc(&CUDA, graph_h.size_in_bytes(), &graph_d));
+    CUDA_CALL(hb_mc_device_malloc(&CUDA, frontier_in_h.size_in_bytes(), &frontier_in_d));
+    CUDA_CALL(hb_mc_device_malloc(&CUDA, frontier_out_h.size_in_bytes(), &frontier_out_d));
+    CUDA_CALL(hb_mc_device_malloc(&CUDA, visited_h.size_in_bytes(), &visited_d));
+
+
+    // copy graph to device
+    hb_mc_dma_htod_t to_device [] = {
+	{
+	    .d_addr = graph_d,
+	    .h_addr = graph_h.data(),
+	    .size   = graph_h.size_in_bytes()
+	},
+	{
+	    .d_addr = frontier_in_d,
+	    .h_addr = frontier_in_h.data(),
+	    .size   = frontier_in_h.size_in_bytes()
+	},
+	{
+	    .d_addr = visited_d,
+	    .h_addr = visited_h.data(),
+	    .size   = visited_h.size_in_bytes()
+	}
+    };
+
+    printf("Copying graph data, input set, and visited array to device\n");
+
+    std::size_t xfer_size_to_device
+	= graph_h.size_in_bytes()
+	+ visited_h.size_in_bytes()
+	+ frontier_in_h.size_in_bytes();
+
+    if (xfer_size_to_device > DMA_SIZE_THRESHOLD)
+	CUDA_CALL(hb_mc_device_dma_to_device(&CUDA, to_device, ARRAY_SIZE(to_device)));
+    else
+	CUDA_CALL(cuda_memcpy_to_device(&CUDA, to_device, ARRAY_SIZE(to_device)));
+    
+    printf("Done!\n");
+
+    printf("Calling %s\n", KERNEL);
+
+    uint32_t kernel_args [] = {
+	graph_d,
+	visited_d,	
+	frontier_in_d,
+	frontier_out_d
+    };
+    CUDA_CALL(hb_mc_kernel_enqueue(&CUDA, GRID, TG,
+				   KERNEL, ARRAY_SIZE(kernel_args), kernel_args));
+    CUDA_CALL(hb_mc_device_tile_groups_execute(&CUDA));
+    printf("Done!\n");
+
+    auto frontier_out_b = frontier_out_h;
+    auto visited_b = visited_h;
+
+    int edges_traversed =
+	bfs_sparse_push(graph_b.data(),
+			visited_b.data(),
+			frontier_in_h.data(),
+			frontier_out_b.data());
+
+    printf("%d traversed edges\n", edges_traversed);
+
+    std::size_t xfer_size_to_host
+	= visited_h.size_in_bytes()
+	+ frontier_out_h.size_in_bytes();
+    
+    // retrieve the output data from the kernel
+    hb_mc_dma_dtoh_t to_host [] = {
+	{
+	    .d_addr = visited_d,
+	    .h_addr = visited_h.data(),
+	    .size   = visited_h.size_in_bytes()
+	},
+	{
+	    .d_addr = frontier_out_d,
+	    .h_addr = frontier_out_h.data(),
+	    .size   = frontier_out_h.size_in_bytes()
+	}
+    };
+
+    printf("Copying output data to host\n");
+    if (xfer_size_to_host > DMA_SIZE_THRESHOLD)
+	CUDA_CALL(hb_mc_device_dma_to_host(&CUDA, to_host, ARRAY_SIZE(to_host)));
+    else
+	CUDA_CALL(cuda_memcpy_to_host(&CUDA, to_host, ARRAY_SIZE(to_device)));
+    
+    // cuda exit
+    CUDA_CALL(hb_mc_device_finish(&CUDA));
+
+    // compare the output
+    for (int v = 0; v < graph_h.n_nodes(); v++) {
+	if (visited_h[v] != visited_b[v]) {
+	    bsg_pr_err("vertex %d: %s visited_h - %s visited_b", v,
+		       visited_h[v] == 1 ? "in" : "not in",
+		       visited_b[v] == 1 ? "in" : "not in");
+	    
+	}
+    }
+
     return HB_MC_SUCCESS;
 }
+					
 
 #ifdef COSIM
 void cosim_main(uint32_t *exit_code, char * args) {
--- examples/graphs/bfs/kernel/v0/kernel.cpp
+++ examples/graphs/bfs/kernel/v0/kernel.cpp
@@ -1,3 +1,50 @@
-extern "C" int kernel()
+
+#include "graph_algorithm/csr_setup_graph_data.hpp"
+#include "threading/local_range.h"
+
+#define BSG_TILE_GROUP_X_DIM bsg_tiles_X
+#define BSG_TILE_GROUP_Y_DIM bsg_tiles_Y
+
+#include "bsg_tile_group_barrier.h"
+
+using formats::csr_blob_header_t;
+
+INIT_TILE_GROUP_BARRIER(r_barrier, c_barrier, 0, bsg_tiles_X-1, 0, bsg_tiles_Y-1)
+
+static void barrier()
 {
+    bsg_tile_group_barrier(&r_barrier, &c_barrier);
+}
+
+extern "C" int kernel(const csr_blob_header_t *graph,
+		      int       *visited,
+		      const int *frontier_in_sparse,
+		      int       *frontier_out_dense)
+{
+    csr_setup_graph_data(graph);    
+    barrier();
+
+    for (int src_i = thread_id(); src_i < NODES; src_i += num_threads()) {
+	// get next source vertex
+	int src = frontier_in_sparse[src_i];
+	if (src == -1) break;
+	// get the neighbors and degree
+	static_assert(sizeof(int) == sizeof(long int));
+	const int *neighbors = (const int*)(&F_NEIGH[F_OFF[src]]);
+	int degree = F_DEGREE[src];
+	// traverse edges
+	for (int dst_i = 0; dst_i < degree; dst_i++) {	    
+	    int dst = neighbors[dst_i];
+	    // have we visited already?
+	    if (visited[dst] != 1) {
+		// push update
+		visited[dst] = 1;
+		frontier_out_dense[dst] = 1;
+	    }
+	}
+    }
+
+    barrier();
+    
+    return 0;
 }
