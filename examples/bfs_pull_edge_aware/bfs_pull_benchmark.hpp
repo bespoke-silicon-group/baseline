332bc4980f83a71aac9b8f3ddfba7f324dc23151
Emily Furst <eafurst@cs.washington.edu>
2020-04-13 09:46:57 -0700

    start of test for edge aware work distr.

---
+++ examples/bfs_pull_edge_aware/bfs_pull_benchmark.hpp
@@ -0,0 +1,16 @@
+#ifndef __BFS_PULL_BENCHMARK_HPP
+#define __BFS_PULL_BENCHMARK_HPP
+
+#include "builtins_hammerblade.h"
+#include "hammerblade/host/arg_parser.hpp"
+#include <string.h>
+#include <stdio.h>
+#include "../common.h"
+
+
+using hammerblade::Device;
+using hammerblade::Vector;
+using hammerblade::GraphHB;
+using hammerblade::GlobalScalar;
+
+#endif
