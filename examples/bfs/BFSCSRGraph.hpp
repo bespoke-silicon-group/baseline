55110c1079d8df7e903cce12e6eb752724c088cc
Max Ruttenberg <mrutt@cs.washington.edu>
2020-04-16 09:33:31 -0700

    BFSCSRGraph a little

--- examples/bfs/BFSCSRGraph.hpp
+++ examples/bfs/BFSCSRGraph.hpp
@@ -21,17 +21,17 @@ public:
         build_edges();
     }
 
-private:
-public:
     void alloc() {
-        _edge_dev = _hb->alloc(sizeof(_ed[0]) * _ed.size());
-        _node_dev = _hb->alloc(sizeof(_nd[0]) * _nd.size());
+        _edge_dev = _hb->alloc(edge_bytes());
+        _node_dev = _hb->alloc(node_bytes());
     }
     void write() {
-        _hb->push_write(_edge_dev, &_ed[0], sizeof(_ed[0]) * _ed.size());
-        _hb->push_write(_node_dev, &_nd[0], sizeof(_nd[0]) * _nd.size());
+        _hb->push_write(_edge_dev, &_ed[0], edge_bytes());
+        _hb->push_write(_node_dev, &_nd[0], node_bytes());
     }
 
+    size_t node_bytes() const { return sizeof(_nd[0]) * _nd.size(); }
+    size_t edge_bytes() const { return sizeof(_ed[0]) * _ed.size(); }
 private:
     std::vector<NodeData> _nd;
     std::vector<int> _ed;
