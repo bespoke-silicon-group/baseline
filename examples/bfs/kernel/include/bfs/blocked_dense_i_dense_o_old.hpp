2fa3a9f00b314d1a8b4890eaf1245e92dbc726d3
Max Ruttenberg <mrutt@cs.washington.edu>
2020-04-19 16:28:49 -0700

    adds an edge-blocking bottom-up bfs

--- examples/bfs/kernel/include/bfs/blocked_dense_i_dense_o_old.hpp
+++ examples/bfs/kernel/include/bfs/blocked_dense_i_dense_o_old.hpp
@@ -11,25 +11,33 @@
 
 namespace bfs {
     template <int BLOCK_SIZE>
-    int bfs_blocked_dense_i_dense_o(int V, int E,
-                                     const node_data_t *rnodes,
-                                     const int *redges,
-                                     int *dense_i,
-                                     int *dense_o,
-                                     int *visited_io)
+    int bfs_blocked_dense_i_dense_o_old(int V, int E,
+                                        const node_data_t *rnodes,
+                                        const int *redges,
+                                        int *dense_i,
+                                        int *dense_o,
+                                        int *visited_io)
     {
         node_data_t lcl_nodes [BLOCK_SIZE];
+        int lcl_dense_o       [BLOCK_SIZE];
         int lcl_visited_io    [BLOCK_SIZE];
         int blk_dst_n = V/BLOCK_SIZE + (V%BLOCK_SIZE == 0 ? 0 : 1);
 
-        for (int blk_dst_i = bsg_id; blk_dst_i < blk_dst_n; blk_dst_i += bsg_tiles_X * bsg_tiles_Y) {
+        for (int blk_dst_i = bsg_id;
+             blk_dst_i < blk_dst_n;
+             blk_dst_i += bsg_tiles_X * bsg_tiles_Y)
+        {
+            // global offset of this block of dest nodes
             int blk_dst_base = blk_dst_i * BLOCK_SIZE;
+
             // load in visited
             memcpy(&lcl_visited_io[0], &visited_io[blk_dst_base],
                    sizeof(lcl_visited_io));
             // load in node info
             memcpy(&lcl_nodes[0], &rnodes[blk_dst_base],
                    sizeof(lcl_nodes));
+            // clear local dense_o
+            memset(&lcl_dense_o[0], 0, sizeof(lcl_dense_o));
 
             for (int dst_i = 0; dst_i < BLOCK_SIZE; dst_i++) {
                 // skip if visited
@@ -47,11 +55,15 @@ namespace bfs {
                     if (dense_i[src] == 1) {
                         // pull update
                         lcl_visited_io[dst_i] = 1;
-                        dense_o[dst] = 1;
+                        lcl_dense_o[dst_i] = 1;
                         break;
                     }
                 }
             }
+
+            // flush dense_o
+            memcpy(&dense_o[blk_dst_base], &lcl_dense_o[0],
+                   sizeof(lcl_dense_o));
 
             memcpy(&visited_io[blk_dst_base], &lcl_visited_io[0],
                    sizeof(lcl_visited_io));
