3a211b3ee3abf1c3c958299e3a68aa4a52766a73
Max Ruttenberg <mrutt@cs.washington.edu>
2020-04-29 12:24:17 -0700

    sparse_i_dense_o new barrier

--- examples/bfs/kernel/sparse_i_dense_o/kernel.cpp
+++ examples/bfs/kernel/sparse_i_dense_o/kernel.cpp
@@ -1,35 +1,19 @@
-/*
- * This kernel prints the Hello World message
- */
-
-// BSG_TILE_GROUP_X_DIM and BSG_TILE_GROUP_Y_DIM must be defined
-// before bsg_manycore.h and bsg_tile_group_barrier.h are
-// included. bsg_tiles_X and bsg_tiles_Y must also be defined for
-// legacy reasons, but they are deprecated.
-
-#define bsg_tiles_X BSG_TILE_GROUP_X_DIM
-#define bsg_tiles_Y BSG_TILE_GROUP_Y_DIM
+#include <bfs/bfs-kernel-prologue-epilogue.hpp>
 #include <bsg_manycore.h>
-#include <bsg_tile_group_barrier.h>
 
-#include <hello_world.hpp>
-
-INIT_TILE_GROUP_BARRIER(bfs_r_barrier, bfs_c_barrier,
-                        0, bsg_tiles_X-1,
-                        0, bsg_tiles_Y-1);
-
+static
 int degree(int v, int V, int E, const int *offsets, const int *edges)
 {
     return (v < (V-1) ? offsets[v+1]-offsets[v] : E-offsets[v]);
 }
+
 
 extern "C" int bfs_sparse_i_dense_o(int V, int E, const int *offsets, const int *edges,
                                     int *sparse_i,
                                     int *dense_o,
                                     int *visited_io)
 {
-    bsg_tile_group_barrier(&bfs_r_barrier, &bfs_c_barrier);
-    bsg_cuda_print_stat_kernel_start();
+    bfs_kernel_prologue();
 
     for (int i = bsg_id; i < V; i += bsg_tiles_X*bsg_tiles_Y) {
         int src = sparse_i[i];
@@ -45,8 +29,7 @@ extern "C" int bfs_sparse_i_dense_o(int V, int E, const int *offsets, const int 
         }
     }
 
-    bsg_tile_group_barrier(&bfs_r_barrier, &bfs_c_barrier);
-    bsg_cuda_print_stat_kernel_end();
+    bfs_kernel_epilogue();
 
     return 0;
 }
