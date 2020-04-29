8bcaeff460b3dfe037552e9b74b8d306b0cf81dd
Max Ruttenberg <mrutt@cs.washington.edu>
2020-04-29 12:23:44 -0700

    dense_i_dense_o_precompute_degree_colocated new barrier

--- examples/bfs/kernel/dense_i_dense_o_precompute_degree_colocated/kernel.cpp
+++ examples/bfs/kernel/dense_i_dense_o_precompute_degree_colocated/kernel.cpp
@@ -7,24 +7,15 @@
 // included. bsg_tiles_X and bsg_tiles_Y must also be defined for
 // legacy reasons, but they are deprecated.
 
-#define bsg_tiles_X BSG_TILE_GROUP_X_DIM
-#define bsg_tiles_Y BSG_TILE_GROUP_Y_DIM
-#include <bsg_manycore.h>
-#include <bsg_tile_group_barrier.h>
+#include <bfs/bfs-kernel-prologue-epilogue.hpp>
 #include "bfs-common.hpp"
-#include <hello_world.hpp>
-
-INIT_TILE_GROUP_BARRIER(bfs_r_barrier, bfs_c_barrier,
-                        0, bsg_tiles_X-1,
-                        0, bsg_tiles_Y-1);
 
 extern "C" int bfs_dense_i_dense_o(int V, int E, const node_data_t *rnode_data, const int *redges,
                                    int *dense_i,
                                    int *dense_o,
                                    int *visited_io)
 {
-    bsg_tile_group_barrier(&bfs_r_barrier, &bfs_c_barrier);
-    bsg_cuda_print_stat_kernel_start();
+    bfs_kernel_prologue();
 
     for (int dst = bsg_id; dst < V; dst += bsg_tiles_X*bsg_tiles_Y) {
         // skip visited
@@ -43,8 +34,6 @@ extern "C" int bfs_dense_i_dense_o(int V, int E, const node_data_t *rnode_data, 
         }
     }
 
-    bsg_tile_group_barrier(&bfs_r_barrier, &bfs_c_barrier);
-    bsg_cuda_print_stat_kernel_end();
-
+    bfs_kernel_epilogue();
     return 0;
 }
