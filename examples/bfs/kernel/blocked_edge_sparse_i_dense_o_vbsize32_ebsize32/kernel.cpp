344241209dcea1f86c1547b93c018cc57e248bf7
Max Ruttenberg <mrutt@cs.washington.edu>
2020-05-05 22:19:31 -0700

    adds some more kernels

---
+++ examples/bfs/kernel/blocked_edge_sparse_i_dense_o_vbsize32_ebsize32/kernel.cpp
@@ -0,0 +1,18 @@
+#include <bfs/bfs-kernel-prologue-epilogue.hpp>
+#include <bfs/blocked_edge_sparse_i_dense_o.hpp>
+
+extern "C" int bfs_blocked_edge_sparse_i_dense_o(int V, int E,
+                                                 const node_data_t *nodes,
+                                                 const int *edges,
+                                                 int *blocked_sparse_i,
+                                                 int *dense_o,
+                                                 int *visited_io)
+{
+    bfs_kernel_prologue();
+    bfs::bfs_blocked_edge_sparse_i_dense_o<32, 32>(V, E, nodes, edges,
+                                                   blocked_sparse_i, dense_o, visited_io);
+
+    bfs_kernel_epilogue();
+
+    return 0;
+}
