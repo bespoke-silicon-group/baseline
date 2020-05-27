1c29ac717ee7d83a36451dde4d77072a769e4e42
Seyed Borna Ehsani <behsani@cs.washington.edu>
2020-05-27 15:33:36 -0700

    fixup

--- examples/shared_mem/kernel/v1/kernel.cpp
+++ examples/shared_mem/kernel/v1/kernel.cpp
@@ -19,24 +19,6 @@
 #include <cstdint>
 #include <shared_mem.hpp>
 
-// I <3 Hacks! Since bsg_manycore_arch.h can't handle the awesomeness
-// of C++ templates I wrote this temporary replacement.
-#ifdef bsg_tile_group_remote_ptr
-#undef bsg_tile_group_remote_ptr
-#include <type_traits>
-#define bsg_tile_group_remote_ptr(__type,x,y,local_addr) \
-        ( (typename std::add_pointer<__type>::type)                     \
-        (   (REMOTE_EPA_PREFIX << REMOTE_EPA_MASK_SHIFTS)               \
-            | ((y) << Y_CORD_SHIFTS )                                   \
-            | ((x) << X_CORD_SHIFTS )                                   \
-            | ((uint32_t) (local_addr)   )                              \
-            )                                                           \
-                                                           )
-#endif
-
-
-
-
 bsg_barrier<bsg_tiles_X, bsg_tiles_Y> barrier;
 
 
