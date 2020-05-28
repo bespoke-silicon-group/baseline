7bf4a091fc50ad84bcedf9a94d366297ce040546
Seyed Borna Ehsani <behsani@cs.washington.edu>
2020-05-27 17:17:46 -0700

    Added shared_mem_load test

---
+++ examples/shared_mem_load/kernel/include/shared_mem_load.hpp
@@ -0,0 +1,22 @@
+#ifndef __SHARED_MEM_LOAD_HPP
+#define __SHARED_MEM_LOAD_HPP
+#include <cstdint>
+
+
+// I <3 Hacks! Since bsg_manycore_arch.h can't handle the awesomeness
+// of C++ templates I wrote this temporary replacement.
+#ifdef bsg_tile_group_remote_ptr
+#undef bsg_tile_group_remote_ptr
+#include <type_traits>
+#define bsg_tile_group_remote_ptr(__type,x,y,local_addr) \
+        ( (typename std::add_pointer<__type>::type)                     \
+        (   (REMOTE_EPA_PREFIX << REMOTE_EPA_MASK_SHIFTS)               \
+            | ((y) << Y_CORD_SHIFTS )                                   \
+            | ((x) << X_CORD_SHIFTS )                                   \
+            | ((uint32_t) (local_addr)   )                              \
+            )                                                           \
+                                                           )
+#endif
+
+
+#endif //__SHARED_MEM_LOAD_HPP
