81c2e60093541b4aab2c3af5f146053e3dd36b65
Emily Furst <eafurst@cs.washington.edu>
2020-07-12 14:27:51 -0700

    suite of different bfs kernels

---
+++ examples/bfs_suite/bfs_pull_benchmark.cpp
@@ -0,0 +1,152 @@
+#include "bfs_pull_benchmark.hpp"
+
+GraphHB edges;
+Vector<int32_t> parent_dev;
+Vector<int32_t> frontier_dev;
+Vector<int32_t> next_frontier_dev;
+
+int launch(int argc, char * argv[]){
+  InputParser input(argc, argv);
+  if(!input.cmdOptionExists("-g") || !input.cmdOptionExists("-f") || !input.cmdOptionExists("-p")){
+  
+    std::cerr << "no input args\n";
+    for(auto i = 0; i < argc; i++) {
+      std::cerr << argv[i] << " ";
+    }
+    std::cerr << std::endl;
+    return 0;
+  }
+  std::string ucode_path = input.getRISCVFile();
+
+  int version = 0;
+  if(ucode_path.find("push") != std::string::npos) {
+    version = 1;
+  }
+  else if(ucode_path.find("beamer") != std::string::npos) {
+    version = 2;
+  }
+
+  std::cerr << "load microcode" << std::endl;
+  hammerblade::builtin_loadMicroCodeFromFile(ucode_path);
+  std::cerr << "load graph" << std::endl;
+
+  std::string graph_f = input.getCmdOption("-g");
+  std::string frontier_f = input.getCmdOption("-f");
+  std::string parent_f = input.getCmdOption("-p");
+  edges = hammerblade::builtin_loadEdgesFromFileToHB (graph_f.c_str()); 
+  std::cerr << "in deg of 0: " << edges.in_degree(0) << "num edges: " << edges.num_edges() << std::endl;
+  //edges = hammerblade::builtin_loadEdgesFromFileToHB ( "/mnt/users/ssd0/homes/eafurst/graphit/test/graphs/graph500.16.16.el") ;
+  Device::Ptr device = Device::GetInstance();
+  std::cerr << "init global arrays" << std::endl;
+  parent_dev = Vector<int32_t>(edges.num_nodes());
+  frontier_dev = Vector<int32_t>(edges.num_nodes());
+  next_frontier_dev = Vector<int32_t>(edges.num_nodes());
+
+  std::vector<int32_t> zeros(edges.num_nodes(), 0);
+  next_frontier_dev.copyToDevice(zeros.data(), zeros.size());
+
+  std::cerr << "Try to insert val to parent" << std::endl;
+  //int * file_frontier = hammerblade::builtin_loadFrontierFromFile(frontier_f.c_str());
+  //int * file_parent = hammerblade::builtin_loadFrontierFromFile(parent_f.c_str());
+  std::cerr << "done with file read" << std::endl;
+
+  //parent_dev.copyToDevice(file_parent, edges.num_nodes());
+  //frontier_dev.copyToDevice(file_frontier, edges.num_nodes());
+  parent_dev.insert(65535, 65535);
+  frontier_dev.insert(65535, 1);
+
+  std::cerr << "doing batch dma write" << std::endl;
+  device->write_dma();
+
+
+  std::cerr << "starting while loop" << std::endl;
+  switch(version){
+    case 0: // do dense pull bfs
+      for(int i = 0; i < 1; i++) //just doing one large iteration
+      {
+        device->enqueueJob("edgeset_apply_pull_parallel_from_vertexset_to_filter_func_with_frontier_call",
+                         hb_mc_dimension(4,4),
+                        {edges.getInIndicesAddr(),
+                         edges.getInNeighborsAddr(),
+                         frontier_dev.getAddr(), next_frontier_dev.getAddr(),  parent_dev.getAddr(),
+                         edges.num_nodes(),
+                         edges.num_edges(),
+                         edges.num_nodes()});
+        device->runJobs();
+        std::cerr << "finished call" << std::endl;
+        //hammerblade::swap_global_arrays<int32_t>(next_frontier_dev, frontier_dev);
+        //hammerblade::assign_val_dma(0, hammerblade::builtin_getVerticesHB(edges), 0, next_frontier_dev);
+        //device->write_dma();
+    }
+    break;
+  case 1: //do sparse push bfs
+      for(int i = 0; i < 1; i++) //just doing one large iteration
+      {
+        device->enqueueJob("edgeset_apply_push_serial_from_vertexset_to_filter_func_with_frontier_call",
+                         hb_mc_dimension(4,4),
+                        {edges.getOutIndicesAddr(),
+                         edges.getOutNeighborsAddr(),
+                         frontier_dev.getAddr(), next_frontier_dev.getAddr(),  parent_dev.getAddr(),
+                         edges.num_nodes(),
+                         edges.num_edges(),
+                         edges.num_nodes()});
+        device->runJobs();
+        std::cerr << "finished call" << std::endl;
+        //hammerblade::swap_global_arrays<int32_t>(next_frontier_dev, frontier_dev);
+        //hammerblade::assign_val_dma(0, hammerblade::builtin_getVerticesHB(edges), 0, next_frontier_dev);
+        //device->write_dma();
+    }
+    break; 
+  case 2: // do beamer's alg bfs
+      for(int i = 0; i < 1; i++) //just doing one large iteration
+      {
+        device->enqueueJob("edgeset_apply_hybrid_parallel_call",
+                         hb_mc_dimension(4,4),
+                        {edges.getOutIndicesAddr(),
+                         edges.getOutNeighborsAddr(),
+                         edges.getInIndicesAddr(),
+                         edges.getInNeighborsAddr(),
+                         frontier_dev.getAddr(), next_frontier_dev.getAddr(),  parent_dev.getAddr(),
+                         edges.num_nodes(),
+                         edges.num_edges(),
+                         edges.num_nodes()});
+        device->runJobs();
+        std::cerr << "finished call" << std::endl;
+        //hammerblade::swap_global_arrays<int32_t>(next_frontier_dev, frontier_dev);
+        //hammerblade::assign_val_dma(0, hammerblade::builtin_getVerticesHB(edges), 0, next_frontier_dev);
+        //device->write_dma();
+    }
+    break;
+  } 
+  std::cerr << "finished while loop" << std::endl;
+  return 0;
+  //hammerblade::read_global_buffer<int32_t>(host_parent, parent_dev, edges.num_nodes());
+  //std::cerr << "Results of BFS" << std::endl;
+  //for(auto i : host_parent) {
+  //    std::cerr << i << std::endl;
+  //}
+}
+#ifdef COSIM
+void cosim_main(uint32_t *exit_code, char * args) {
+    int argc = get_argc(args);
+    char *argv[argc];
+    get_argv(args, argc, argv);
+#ifdef VCS
+    svScope scope;
+    scope = svGetScopeFromName("tb");
+    svSetScope(scope);
+#endif
+    bsg_pr_test_info("Unified Main Regression Test (COSIMULATION)\n");
+    int rc = launch(argc,argv);
+    *exit_code = rc;
+    bsg_pr_test_pass_fail(rc == HB_MC_SUCCESS);
+    return;
+}
+#else
+int main(int argc, char ** argv) {
+    bsg_pr_test_info("Unified Main CUDA Regression Test (F1)\n");
+    int rc = launch(argc,argv);
+    bsg_pr_test_pass_fail(rc == HB_MC_SUCCESS);
+    return rc;
+}
+#endif
---
+++ examples/bfs_suite/kernel/beamer/kernel.cpp
@@ -0,0 +1,161 @@
+// BSG_TILE_GROUP_X_DIM and BSG_TILE_GROUP_Y_DIM must be defined
+// before bsg_manycore.h and bsg_tile_group_barrier.h are
+// included. bsg_tiles_X and bsg_tiles_Y must also be defined for
+// legacy reasons, but they are deprecated.
+#define BSG_TILE_GROUP_X_DIM 4
+#define BSG_TILE_GROUP_Y_DIM 4
+#define bsg_tiles_X BSG_TILE_GROUP_X_DIM
+#define bsg_tiles_Y BSG_TILE_GROUP_Y_DIM
+#include <bsg_manycore.h>
+#include <bsg_tile_group_barrier.hpp>
+bsg_barrier<bsg_tiles_X, bsg_tiles_Y> barrier;
+#include <bfs_pull_benchmark.hpp>
+#include <cstring>
+#include <local_range.h>
+#include <calculate_direction.h>
+
+#ifdef DEBUG
+#define pr_dbg(fmt, ...)                        \
+    bsg_printf(fmt, ##__VA_ARGS__)
+#else
+#define pr_dbg(fmt, ...)
+#endif
+
+template <typename TO_FUNC , typename APPLY_FUNC> int edgeset_apply_push_serial_from_vertexset_to_filter_func_with_frontier(int *out_indices , int *out_neighbors, int* from_vertexset, int * next_frontier, int * parent, TO_FUNC to_func, APPLY_FUNC apply_func, int V, int E, int block_size_x)
+{   
+    int start, end;
+    local_range(V, &start, &end);
+    for ( int s=start; s < end; s++) {
+      if(from_vertexset[s]) {
+        int degree = out_indices[s + 1] - out_indices[s];
+        int * neighbors = &out_neighbors[out_indices[s]];
+        for(int d = 0; d < degree; d++) {
+          if(parent[neighbors[d]] == -1) {
+            parent[neighbors[d]] = s;
+            next_frontier[neighbors[d]] = 1;
+          } //end of to func
+        } //end of for loop on neighbors
+      }
+    }
+    //barrier.sync();
+    return 0;
+} //end of edgeset apply function
+
+template <typename TO_FUNC , typename APPLY_FUNC> int edgeset_apply_pull_parallel_from_vertexset_to_filter_func_with_frontier(int *in_indices , int *in_neighbors, int * frontier, int * next_frontier, int * parent, TO_FUNC to_func, APPLY_FUNC apply_func, int V, int E, int block_size_x) 
+{
+
+  int start, end;
+  local_range(V, &start, &end);
+  for ( int d = start; d < end; d++) {
+        if (parent[d] == -1){ 
+          int degree = in_indices[d + 1] - in_indices[d];
+          int * neighbors = &in_neighbors[in_indices[d]];
+          for(int s = 0; s < degree; s++) {
+            int src = neighbors[s];
+            if(frontier[src] == 1) {
+              parent[d] = src;
+              next_frontier[d] = 1;
+	      			break;
+            }
+          } //end of loop on in neighbors
+        } //end of to filtering
+  } //end of outer for loop
+  return 0;
+} //end of edgeset apply function 
+
+
+struct parent_generated_vector_op_apply_func_0
+{
+  void operator() (int v, int * parent)
+  {
+    parent[v] =  -(1) ;
+  };
+};
+struct updateEdge
+{
+  bool operator() (int src, int dst, int * parent)
+  {
+    bool output1 ;
+    parent[dst] = src;
+    output1 = (bool) 1;
+    return output1;
+  };
+};
+struct toFilter
+{
+  bool operator() (int v, int * parent)
+  {
+    bool output ;
+    output = (parent[v]) == ( -(1) );
+    return output;
+  };
+};
+struct reset
+{
+  void operator() (int v, int * parent)
+  {
+    parent[v] =  -(1) ;
+  };
+};
+
+extern "C" int  __attribute__ ((noinline)) parent_generated_vector_op_apply_func_0_kernel(int * parent, int V, int E, int block_size_x) {
+	int start_x = block_size_x * (__bsg_tile_group_id_y * __bsg_grid_dim_x + __bsg_tile_group_id_x);
+	for (int iter_x = __bsg_id; iter_x < block_size_x; iter_x += bsg_tiles_X * bsg_tiles_Y) {
+		if ((start_x + iter_x) < V) {
+			parent_generated_vector_op_apply_func_0()(start_x + iter_x, parent);
+		}
+		else {
+			break;
+		}
+	}
+	barrier.sync();
+  return 0;
+}
+extern "C" int  __attribute__ ((noinline)) reset_kernel(int * parent, int V, int E, int block_size_x) {
+	int start_x = block_size_x * (__bsg_tile_group_id_y * __bsg_grid_dim_x + __bsg_tile_group_id_x);
+	for (int iter_x = __bsg_id; iter_x < block_size_x; iter_x += bsg_tiles_X * bsg_tiles_Y) {
+		if ((start_x + iter_x) < V) {
+			reset()(start_x + iter_x, parent);
+		}
+		else {
+			break;
+		}
+	}
+  barrier.sync();
+	return 0;
+}
+extern "C" int __attribute__ ((noinline)) edgeset_apply_pull_parallel_from_vertexset_to_filter_func_with_frontier_call(int *in_indices, int *in_neighbors, int * frontier, int * next_frontier, int * parent, int V, int E, int block_size_x) {
+  barrier.sync();
+  bsg_cuda_print_stat_start(1);
+	edgeset_apply_pull_parallel_from_vertexset_to_filter_func_with_frontier(in_indices, in_neighbors, frontier, next_frontier, parent, toFilter(), updateEdge(), V, E, block_size_x);
+	bsg_cuda_print_stat_end(1);
+	barrier.sync();
+	return 0;
+}
+
+extern "C" int __attribute__ ((noinline)) edgeset_apply_push_serial_from_vertexset_to_filter_func_with_frontier_call(int *out_indices, int *out_neighbors, int *frontier, int *next_frontier, int *parent, int V, int E, int block_size_x) {
+  barrier.sync();
+  bsg_cuda_print_stat_start(1);
+  edgeset_apply_push_serial_from_vertexset_to_filter_func_with_frontier(out_indices, out_neighbors, frontier, next_frontier, parent, toFilter(), updateEdge(), V, E, block_size_x);
+  bsg_cuda_print_stat_end(1);
+  barrier.sync();
+  return 0;
+}
+
+extern "C" int __attribute__ ((noinline)) edgeset_apply_hybrid_parallel_call(int *out_indices, int *out_neighbors, int * in_indices, int * in_neighbors, int *frontier, int *next_frontier, int *parent, int V, int E, int block_size_x) { 
+  barrier.sync();
+  if(bsg_id == 1) bsg_printf("calculate direction: \n");
+  int pull = calculate_direction(frontier, out_indices, V, E);
+  bsg_cuda_print_stat_start(1);
+  if(pull == 1) {
+    if(bsg_id == 1) bsg_printf("pull direction\n");
+	  edgeset_apply_pull_parallel_from_vertexset_to_filter_func_with_frontier(in_indices, in_neighbors, frontier, next_frontier, parent, toFilter(), updateEdge(), V, E, block_size_x);
+  }
+  else {
+    if(bsg_id == 1) bsg_printf("push direction\n");
+    edgeset_apply_push_serial_from_vertexset_to_filter_func_with_frontier(out_indices, out_neighbors, frontier, next_frontier, parent, toFilter(), updateEdge(), V, E, block_size_x);
+  }
+  bsg_cuda_print_stat_end(1);
+  barrier.sync();
+  return 0;
+}
