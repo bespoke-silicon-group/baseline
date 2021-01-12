c3e750a14599a9b93babe39ce6735d81d8118b01
Max Ruttenberg <mrutt@cs.washington.edu>
2021-01-12 14:15:46 -0800

    Cleans host code

--- examples/ipnsw/ipnsw.cpp
+++ examples/ipnsw/ipnsw.cpp
@@ -4,83 +4,19 @@
 #include "Graph.hpp"
 #include "IO.hpp"
 #include "IPNSWGraph.hpp"
+#include "IPNSWRunner.hpp"
 #include <iostream>
-
+#include <memory>
 using namespace ipnsw;
-
-using hammerblade::host::HammerBlade;
-using hammerblade::host::Dim;
-
-HammerBlade::Ptr hb;
-
-constexpr int QUERY = 276; // fewest inner products in greedy walk
 
 int Main(int argc, char *argv[])
 {
     Parser args;
     args.parse(argc, argv);
 
-    IO io(args);
-    auto graphs = io.graphs();
-    auto db = io.database<float,100>();
-    auto queries = io.queries<float,100>();
-
-    std::cout << "Read in " << db.size() << " vectors" << std::endl;
-    std::cout << "Graph 0: " << graphs[0].num_nodes() << " nodes" << std::endl;
-    std::cout << "Graph 0: " << graphs[0].num_edges() << " edges" << std::endl;
-
-    std::vector<Graph> Gs = {
-        Graph(std::move(graphs[3])),
-        Graph(std::move(graphs[2])),
-        Graph(std::move(graphs[1])),
-        Graph(std::move(graphs[0]))
-    };
-
-#if 1
-    hb = HammerBlade::Get();
-    hb->load_application(args._ucode);
-
-    std::cout << "Allocating database " << std::endl;
-    hb_mc_eva_t db_dev = hb->alloc(db.size() * sizeof(db[0]));
-    std::cout << "Allocating query "  << std::endl;
-    hb_mc_eva_t q_dev  = hb->alloc(sizeof(queries[0]));
-    std::cout << "Allocating seen set " << std::endl;
-    hb_mc_eva_t seen_dev = hb->alloc(db.size() * sizeof(int));
-
-    // Return values
-    hb_mc_eva_t v_curr_dev = hb->alloc(sizeof(int));
-    hb_mc_eva_t d_curr_dev = hb->alloc(sizeof(float));
-
-    std::cout << "Pushing write jobs" << std::endl;
-    hb->push_write(db_dev, &db[0], db.size() * sizeof(db[0]));
-    hb->push_write(q_dev,  &queries[QUERY], sizeof(queries[QUERY]));
-
-    for (auto &g : Gs) g.initialize_on_device();
-    // syncs
-    hb_mc_eva_t graph_metadata_dev = Graph::InitializeMetadataOnDevice(Gs);
-
-    std::cout << "Starting DMA" << std::endl;
-    //hb->sync_write();
-    std::cout << "Launching kernel" << std::endl;
-    //hb->push_job(Dim(1,1), Dim(1,1), "inner_product_ubmk", db_dev, q_dev);
-    hb->push_job(Dim(1,1), Dim(1,1), "ipnsw_greedy_search",
-                 graph_metadata_dev, db_dev, q_dev, seen_dev,
-                 v_curr_dev, d_curr_dev);
-
-    //hb->push_job(Dim(1,1), Dim(1,1), "input_test", graph_metadata_dev, db_dev, q_dev, seen_dev);
-    hb->exec();
-
-    int v_curr;
-    float d_curr;
-    hb->read(v_curr_dev, &v_curr, sizeof(v_curr));
-    hb->read(d_curr_dev, &d_curr, sizeof(d_curr));
-
-    std::cout << "Greedy walk (v_curr,d_curr) = ("
-              <<  v_curr << "," << d_curr
-              << ")" << std::endl;
-
-#endif
-    delete hb;
+    std::unique_ptr<IPNSWRunner> runner;
+    runner = std::unique_ptr<IPNSWRunner>(new IPNSWRunner(args));
+    runner->run();
 
     return 0;
 }
