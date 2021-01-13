c580e689a2266a0c36ca026129a0f4797b437ba7
Max Ruttenberg <mrutt@cs.washington.edu>
2021-01-13 15:09:31 -0800

    Fixes typo in result reader

--- examples/ipnsw/IProductUBmkResultReader.hpp
+++ examples/ipnsw/IProductUBmkResultReader.hpp
@@ -6,7 +6,7 @@ namespace ipnsw {
     class IProductUBmkResultReader : public IPNSWResultReader {
     public:
         void readResults(const IPNSWRunner & runner) {
-            std::cout << "Done" << std:endl;
+            std::cout << "Done" << std::endl;
         }
     };
 }
