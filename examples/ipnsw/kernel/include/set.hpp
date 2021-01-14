66cf8dcc73d093359b7b5c2fde179afab2f6cb22
Max Ruttenberg <mrutt@cs.washington.edu>
2021-01-14 15:20:32 -0800

    Adds missing header set.hpp

---
+++ examples/ipnsw/kernel/include/set.hpp
@@ -0,0 +1,28 @@
+#pragma once
+#include <algorithm>
+template<typename T, typename Comparitor>
+class DynSet {
+public:
+    DynSet(T *data, int N):
+        _data(data),
+        _data_N(N),
+        _n(0) {
+    }
+
+    void insert(T i) {
+        _data[_n++] = i;
+        std::sort(_data, _data+_n, Comparitor());
+    }
+
+    bool in(T i) {
+        return std::binary_search(_data, _data+_n, i, Comparitor());
+    }
+
+    int size() const {
+        return _n;
+    }
+
+    T    *_data;
+    int   _n;
+    int   _data_N;
+};
