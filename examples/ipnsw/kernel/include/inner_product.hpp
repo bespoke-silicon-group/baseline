729d80ac12afbe01163a55285d60a2752c7da32a
Max Ruttenberg <mrutt@cs.washington.edu>
2021-01-15 18:23:57 -0800

    Explicitly parallel FMAs

--- examples/ipnsw/kernel/include/inner_product.hpp
+++ examples/ipnsw/kernel/include/inner_product.hpp
@@ -47,3 +47,21 @@ FLOAT_T inner_product_v2(const FLOAT_T *__restrict a,
     }
     return r;
 }
+
+
+
+template<std::size_t TG_X, std::size_t TG_Y, typename FLOAT_T=float, std::size_t VSIZE=100, std::size_t BSIZE=10>
+__attribute__((noinline))
+FLOAT_T inner_product_v3(const FLOAT_T *__restrict a,
+                         bsg_attr_remote const FLOAT_T *__restrict b)
+{
+    FLOAT_T r0 = 0.0, r1 = 0.0;
+    for (int i = __bsg_id * BSIZE; i < VSIZE; i += 2 * BSIZE * TG_X * TG_Y) {
+#pragma bsg_unroll(32)
+        for (int j = 0; j < BSIZE; ++j) {
+            r0 = fmaf(a[i+j], b[i+j], r0);
+            r1 = fmaf(a[i+j+BSIZE], b[i+j+BSIZE], r1);
+        }
+    }
+    return r0+r1;
+}
