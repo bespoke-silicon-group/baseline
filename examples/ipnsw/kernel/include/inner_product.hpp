178a7d8c2e60917dee09e9d2c419b5dd06b1156e
Max Ruttenberg <mrutt@cs.washington.edu>
2021-01-15 23:12:06 -0800

    Adds an unrolled factor of 4 dot product.

--- examples/ipnsw/kernel/include/inner_product.hpp
+++ examples/ipnsw/kernel/include/inner_product.hpp
@@ -59,9 +59,28 @@ FLOAT_T inner_product_v3(const FLOAT_T *__restrict a,
     for (int i = __bsg_id * BSIZE; i < VSIZE; i += 2 * BSIZE * TG_X * TG_Y) {
 #pragma bsg_unroll(32)
         for (int j = 0; j < BSIZE; ++j) {
-            r0 = fmaf(a[i+j], b[i+j], r0);
-            r1 = fmaf(a[i+j+BSIZE], b[i+j+BSIZE], r1);
+            r0 = fmaf(a[i+j+0*BSIZE], b[i+j+0*BSIZE], r0);
+            r1 = fmaf(a[i+j+1*BSIZE], b[i+j+1*BSIZE], r1);
         }
     }
     return r0+r1;
+}
+
+template<std::size_t TG_X, std::size_t TG_Y, typename FLOAT_T=float, std::size_t VSIZE=100, std::size_t BSIZE=5>
+__attribute__((noinline))
+FLOAT_T inner_product_v4(const FLOAT_T *__restrict a,
+                         bsg_attr_remote const FLOAT_T *__restrict b)
+{
+    FLOAT_T r0 = 0.0, r1 = 0.0;
+    FLOAT_T r2 = 0.0, r3 = 0.0;
+    for (int i = __bsg_id * BSIZE; i < VSIZE; i += 4 * BSIZE * TG_X * TG_Y) {
+#pragma bsg_unroll(32)
+        for (int j = 0; j < BSIZE; ++j) {
+            r0 = fmaf(a[i+j+0*BSIZE], b[i+j+0*BSIZE], r0);
+            r1 = fmaf(a[i+j+1*BSIZE], b[i+j+1*BSIZE], r1);
+            r2 = fmaf(a[i+j+2*BSIZE], b[i+j+2*BSIZE], r2);
+            r3 = fmaf(a[i+j+3*BSIZE], b[i+j+3*BSIZE], r3);
+        }
+    }
+    return r0+r1+r2+r3;
 }
