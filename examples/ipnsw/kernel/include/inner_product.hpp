7c5029cfbaa68ebb55e578cc4c82808b09d9cbc7
Max Ruttenberg <mrutt@cs.washington.edu>
2021-01-18 07:57:23 -0800

    Attempts a parameterizable ILP heavy inner product (v4)

--- examples/ipnsw/kernel/include/inner_product.hpp
+++ examples/ipnsw/kernel/include/inner_product.hpp
@@ -1,6 +1,7 @@
 #pragma once
 #include "bsg_striped_array.hpp"
 #include <cmath>
+#include <numeric>
 
 template<std::size_t TG_X, std::size_t TG_Y, typename FLOAT_T=float, std::size_t VSIZE=100, std::size_t BSIZE=10>
 __attribute__((noinline))
@@ -66,21 +67,23 @@ FLOAT_T inner_product_v3(const FLOAT_T *__restrict a,
     return r0+r1;
 }
 
-template<std::size_t TG_X, std::size_t TG_Y, typename FLOAT_T=float, std::size_t VSIZE=100, std::size_t BSIZE=5>
+template<std::size_t TG_X, std::size_t TG_Y, typename FLOAT_T=float, std::size_t VSIZE=100, std::size_t BSIZE=5, int UNROLL=5>
 __attribute__((noinline))
 FLOAT_T inner_product_v4(const FLOAT_T *__restrict a,
                          bsg_attr_remote const FLOAT_T *__restrict b)
 {
-    FLOAT_T r0 = 0.0, r1 = 0.0;
-    FLOAT_T r2 = 0.0, r3 = 0.0;
-    for (int i = __bsg_id * BSIZE; i < VSIZE; i += 4 * BSIZE * TG_X * TG_Y) {
+    register FLOAT_T r[UNROLL];
+    for (int i = __bsg_id * BSIZE; i < VSIZE; i += UNROLL * BSIZE * TG_X * TG_Y) {
 #pragma bsg_unroll(32)
         for (int j = 0; j < BSIZE; ++j) {
-            r0 = fmaf(a[i+j+0*BSIZE], b[i+j+0*BSIZE], r0);
-            r1 = fmaf(a[i+j+1*BSIZE], b[i+j+1*BSIZE], r1);
-            r2 = fmaf(a[i+j+2*BSIZE], b[i+j+2*BSIZE], r2);
-            r3 = fmaf(a[i+j+3*BSIZE], b[i+j+3*BSIZE], r3);
+#pragma bsg_unroll(32)
+            for (int k =0 ; k < UNROLL; ++k) {
+                r[k] = fmaf(a[i+j+k*BSIZE], b[i+j+k*BSIZE], r[k]);
+            }
         }
     }
-    return r0+r1+r2+r3;
+    int rs = 0.0;
+    for (int i = 0; i < UNROLL; ++i)
+        rs += r[i];
+    return rs;
 }
